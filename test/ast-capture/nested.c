f51af18aca2b1c5a60da28c4d5463bebd8ba1da0
Norman Rink <norman.rink@tu-dresden.de>
2016-04-28 11:37:08 +0200

    Added 'lit' commands to tests.

--- test/ast-capture/nested.c
+++ test/ast-capture/nested.c
@@ -1,4 +1,9 @@
 
+// RUN: %clang -o %t1 %s
+// RUN: %t1 || echo $? | grep 9
+
+// RUN: %clang -c -Xclang -ast-dump %s | grep --after-context=14 VarDecl | grep --after-context=14 res | FileCheck --check-prefix=VAR-DECL-res %s
+
 int y = 42;
 
 $$[Expr] add (Expr[int] x $ Expr[int] y)
@@ -16,5 +21,21 @@ int main() {
   int y = 2;
 
   int res = ($fun(x $ y));
+// VAR-DECL-res:          | `-VarDecl 0x{{[0-9a-f]+}} <col:{{[0-9]+}}, col:{{[0-9]+}}> col:{{[0-9]+}} used res 'int' cinit
+// VAR-DECL-res-NEXT:     |   `-ParenExpr 0x{{[0-9a-f]+}} <col:{{[0-9]+}}, col:{{[0-9]+}}> 'int'
+// VAR-DECL-res-NEXT:     |     `-BinaryOperator 0x{{[0-9a-f]+}} <line:{{[0-9]+}}:{{[0-9]+}}, col:{{[0-9]+}}> 'int' '+'
+// VAR-DECL-res-NEXT:     |       |-ParenExpr 0x{{[0-9a-f]+}} <col:{{[0-9]+}}, col:{{[0-9]+}}> 'int'
+// VAR-DECL-res-NEXT:     |       | `-ImplicitCastExpr 0x{{[0-9a-f]+}} <line:{{[0-9]+}}:{{[0-9]+}}> 'int' <LValueToRValue>
+// VAR-DECL-res-NEXT:     |       |   `-DeclRefExpr 0x{{[0-9a-f]+}} <col:{{[0-9]+}}> 'int' lvalue Var 0x{{[0-9a-f]+}} 'x' 'int'
+// VAR-DECL-res-NEXT:     |       `-ParenExpr 0x{{[0-9a-f]+}} <line:{{[0-9]+}}:{{[0-9]+}}, col:{{[0-9]+}}> 'int'
+// VAR-DECL-res-NEXT:     |         `-ParenExpr 0x{{[0-9a-f]+}} <line:{{[0-9]+}}:{{[0-9]+}}, col:{{[0-9]+}}> 'int'
+// VAR-DECL-res-NEXT:     |           `-BinaryOperator 0x{{[0-9a-f]+}} <col:{{[0-9]+}}, col:{{[0-9]+}}> 'int' '*'
+// VAR-DECL-res-NEXT:     |             |-ParenExpr 0x{{[0-9a-f]+}} <col:{{[0-9]+}}, col:{{[0-9]+}}> 'int'
+// VAR-DECL-res-NEXT:     |             | `-ImplicitCastExpr 0x{{[0-9a-f]+}} <line:{{[0-9]+}}:{{[0-9]+}}> 'int' <LValueToRValue>
+// VAR-DECL-res-NEXT:     |             |   `-DeclRefExpr 0x{{[0-9a-f]+}} <col:{{[0-9]+}}> 'int' lvalue Var 0x{{[0-9a-f]+}} 'x' 'int'
+// VAR-DECL-res-NEXT:     |             `-ParenExpr 0x{{[0-9a-f]+}} <line:{{[0-9]+}}:{{[0-9]+}}, col:{{[0-9]+}}> 'int'
+// VAR-DECL-res-NEXT:     |               `-ImplicitCastExpr 0x{{[0-9a-f]+}} <line:{{[0-9]+}}:{{[0-9]+}}> 'int' <LValueToRValue>
+// VAR-DECL-res-NEXT:     |                 `-DeclRefExpr 0x{{[0-9a-f]+}} <col:{{[0-9]+}}> 'int' lvalue Var 0x{{[0-9a-f]+}} 'y' 'int'
+
   return res; // return '9'
 }
--- test/ast-capture/types.c
+++ test/ast-capture/types.c
@@ -1,4 +1,10 @@
 
+// RUN: %clang -o %t1 %s
+// RUN: %t1 || echo $? | grep 85
+
+// RUN: %clang -c -Xclang -ast-dump %s | grep --after-context=10 WhileStmt | FileCheck --check-prefix=WHILE %s
+// RUN: %clang -c -Xclang -ast-dump %s | grep --after-context=7 ReturnStmt | FileCheck --check-prefix=RETURN %s
+
 int y = 42;
 
 $$[Stmt] whileinc(Expr[int] VAL $ CompoundAssignOperator[int] INC)
@@ -17,5 +23,25 @@ int main() {
     ;
 
   $fun(x $ x+=1)
+// WHILE:          |-WhileStmt 0x{{[0-9a-f]+}} <line:{{[0-9]+}}:{{[0-9]+}}, line:{{[0-9]+}}:{{[0-9]+}}>
+// WHILE-NEXT:     | |-<<<NULL>>>
+// WHILE-NEXT:     | |-BinaryOperator 0x{{[0-9a-f]+}} <line:{{[0-9]+}}:{{[0-9]+}}, col:{{[0-9]+}}> 'int' '<'
+// WHILE-NEXT:     | | |-ParenExpr 0x{{[0-9a-f]+}} <col:{{[0-9]+}}, col:{{[0-9]+}}> 'int'
+// WHILE-NEXT:     | | | `-ImplicitCastExpr 0x{{[0-9a-f]+}} <line:{{[0-9]+}}:{{[0-9]+}}> 'int' <LValueToRValue>
+// WHILE-NEXT:     | | |   `-DeclRefExpr 0x{{[0-9a-f]+}} <col:{{[0-9]+}}> 'int' lvalue Var 0x{{[0-9a-f]+}} 'x' 'int'
+// WHILE-NEXT:     | | `-ImplicitCastExpr 0x{{[0-9a-f]+}} <line:{{[0-9]+}}:{{[0-9]+}}> 'int' <LValueToRValue>
+// WHILE-NEXT:     | |   `-DeclRefExpr 0x{{[0-9a-f]+}} <col:{{[0-9]+}}> 'int' lvalue Var 0x{{[0-9a-f]+}} 'y' 'int'
+// WHILE-NEXT:     | `-CompoundAssignOperator 0x{{[0-9a-f]+}} <line:{{[0-9]+}}:{{[0-9]+}}, col:{{[0-9]+}}> 'int' '+=' ComputeLHSTy='int' ComputeResultTy='int'
+// WHILE-NEXT:     |   |-DeclRefExpr 0x{{[0-9a-f]+}} <col:{{[0-9]+}}> 'int' lvalue Var 0x{{[0-9a-f]+}} 'x' 'int'
+// WHILE-NEXT:     |   `-IntegerLiteral 0x{{[0-9a-f]+}} <col:{{[0-9]+}}> 'int' 1
+
   $ret(x+1+x) // return '85'
+// RETURN:          `-ReturnStmt 0x{{[0-9a-f]+}} <line:{{[0-9]+}}:{{[0-9]+}}, line:{{[0-9]+}}:{{[0-9]+}}>
+// RETURN-NEXT:       `-BinaryOperator 0x{{[0-9a-f]+}} <col:{{[0-9]+}}, col:{{[0-9]+}}> 'int' '+'
+// RETURN-NEXT:         |-BinaryOperator 0x{{[0-9a-f]+}} <col:{{[0-9]+}}, col:{{[0-9]+}}> 'int' '+'
+// RETURN-NEXT:         | |-ImplicitCastExpr 0x{{[0-9a-f]+}} <col:{{[0-9]+}}> 'int' <LValueToRValue>
+// RETURN-NEXT:         | | `-DeclRefExpr 0x{{[0-9a-f]+}} <col:{{[0-9]+}}> 'int' lvalue Var 0x{{[0-9a-f]+}} 'x' 'int'
+// RETURN-NEXT:         | `-IntegerLiteral 0x{{[0-9a-f]+}} <col:{{[0-9]+}}> 'int' 1
+// RETURN-NEXT:         `-ImplicitCastExpr 0x{{[0-9a-f]+}} <col:{{[0-9]+}}> 'int' <LValueToRValue>
+// RETURN-NEXT:           `-DeclRefExpr 0x{{[0-9a-f]+}} <col:{{[0-9]+}}> 'int' lvalue Var 0x{{[0-9a-f]+}} 'x' 'int'
 }
